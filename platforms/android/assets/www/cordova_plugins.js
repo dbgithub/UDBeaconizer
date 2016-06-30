f1a8e181105b561fd6988564f39a52b624b781aa
Aitor <aitor.db@opendeusto.es>
2016-06-30 10:39:25 +0200

    cli de phonegap a 5.1.2 me parece y he añadido el plugin -ble indicando que el source ha de ser NPM y lo mismo en el de eddystone.

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,58 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-ble/ble.js",
+        "id": "cordova-plugin-ble.BLE",
+        "clobbers": [
+            "evothings.ble"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "id": "cordova-plugin-dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-eddystone/js/eddystone-plugin.js",
+        "id": "cordova-plugin-eddystone.eddystoneplugin",
+        "clobbers": [
+            "evothings.eddystone"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-x-toast/www/Toast.js",
+        "id": "cordova-plugin-x-toast.Toast",
+        "clobbers": [
+            "window.plugins.toast"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-x-toast/test/tests.js",
+        "id": "cordova-plugin-x-toast.tests"
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-ble": "1.0.1",
+    "cordova-plugin-dialogs": "1.2.1",
+    "cordova-plugin-eddystone": "1.2.0",
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-x-toast": "2.5.1"
+};
+// BOTTOM OF METADATA
+});
